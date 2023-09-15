0fcc60b3fbd7cc16a48f4808b985e54a6b0ccd85
Dylan Bailey <wearesleepless@gmail.com>
2023-09-15 11:54:45 -0400

    Final logic

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 const inputEl = document.getElementById("input-el")
 const btnEl = document.getElementById("convert-btn")
-const card1 = document.getElementById("meter")
+const lengthEl = document.getElementById("length-p")
+const volumeEl = document.getElementById("volume-p")
+const massEl = document.getElementById("mass-p")
+
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
+
+convertUnit(0)
 
 btnEl.addEventListener("click", function() {
-    card1.innerHTML = `${inputEl.value} meters = ${inputEl.value * 3.281} feet | ${inputEl.value} feet = ${inputEl.value / 3.281}`
+    let value = Number(inputEl.value)
+    convertUnit(value)
 })
+
+function convertUnit(value) {
+    let feet = (value * meterToFeet).toFixed(3)
+    let meters = (value / meterToFeet).toFixed(3)
+    lengthEl.innerHTML = `${value} meters = ${feet} feet | ${value} feet = ${meters} meters`
+    let liter = (value * literToGallon).toFixed(3)
+    let gallon = (value / literToGallon).toFixed(3)
+    volumeEl.innerHTML = `${value} liters = ${liter} gallons | ${value} gallons = ${gallon} liters`
+    let pounds = (value * kiloToPound).toFixed(3)
+    let kilos = (value / kiloToPound).toFixed(3)
+    massEl.innerHTML = `${value} kilos = ${pounds} pounds | ${value} pounds = ${kilos} kilos`
+}
